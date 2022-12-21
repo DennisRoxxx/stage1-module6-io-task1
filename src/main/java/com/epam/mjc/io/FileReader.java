b2d51467081125c771f54deef69dee75f35f2b67
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-21 14:35:27 +0300

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,30 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> profileInfo = new HashMap<>(4);
+        Profile profile = new Profile();
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
+            for (int i = 1; i <= 4; i++) {
+                String[] keyWord = bufferedReader.readLine().split(": ");
+                profileInfo.put(keyWord[0], keyWord[1]);
+            }
+            profile.setName(profileInfo.get("Name"));
+            profile.setAge(Integer.parseInt(profileInfo.get("Age")));
+            profile.setEmail(profileInfo.get("Email"));
+            profile.setPhone(Long.parseLong(profileInfo.get("Phone")));
+            return profile;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
